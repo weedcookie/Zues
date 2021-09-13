4b39615e32c7a1c55fd51cf465924cf7ca9cb524
weedcookie <50777877+weedcookie@users.noreply.github.com>
2021-09-13 06:52:31 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,42 +2,17 @@
 
 
 
-
+# API keys for the Binance account
 pub = ""
-
 sec = ""
 
 
-
-
+# coinpairs to trade on the binance platform
 portfolio = ['BNBBUSD', 'BTCBUSD','BTCRUB','BNBBTC','BNBETH'] 
 
-
-
-
-
-
-
-
-
-
+# gmail creds to send order info via email
 usr = ""
-
 passwrd =  ""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
